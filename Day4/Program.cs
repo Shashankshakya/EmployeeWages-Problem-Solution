a309cf67ab0a49f97bc6216ac8e5052801b24611
shash <shash@DESKTOP-JDS1LMA>
2021-09-10 07:23:38 +0530

    Add project files.

---
+++ Day4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+       public static void Main()
+        {
+            UC1_EmployeeAttendance.GetAttendance();
+            UC2_EmpWage.EmpWage();
+            UC3_PartTimeEmpWage.PartTimeEmpWage();
+            UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            UC8_ComputeEmpWageForMultipleCompanies.Main8("TCS", 100, 22, 2200);
+              Console.ReadKey();
+            
+        }
+    }
+}
